ef8483fc54244cd48d4ce5e23f628819b47c064b
DomYoumans <ddyoumans@icloud.com>
2019-11-15 08:38:35 -0800

    add personalization of template, add input field, add wireframing to main.js

---
+++ src/main.js
@@ -0,0 +1,9 @@
+// Frontend Logic
+import { } from './../src/template.js'
+
+
+$(document).ready(function(){
+  $("#form").submit(function(event){
+    event.preventDefault();
+  })
+})
---
+++ src/template.js
@@ -0,0 +1,3 @@
+// Backend Logic
+
+export function 
