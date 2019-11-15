f9af279afef4e73139e9b03772695fe880259c76
DomYoumans <ddyoumans@icloud.com>
2019-11-15 14:43:46 -0800

    add tests modification

--- __tests__/template.test.js
+++ __tests__/template.test.js
@@ -1,12 +1,8 @@
 import { Calculator } from './../src/calculator.js';
 
-
-
-
-
-  describe("Calculator", () => {
-    test("should show the users current age.", ( => {
+describe("Calculator", () => {
+  test("should show the users current age.", ( => {
     let enteredDate = this.birthday * 365;
     expect(enteredDate.userAge()).toEqual(birthday);
-    });
   });
+});
